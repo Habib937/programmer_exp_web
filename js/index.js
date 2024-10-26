8f42df8bf1c612265ca0c2414d53fe6208d5d16c
1987_ Mohammad Habib Ullah <mohammadhabibullah937@gmail.com>
2024-10-27 02:47:11 +0600

    when history button clicked expence form are vanish

--- js/index.js
+++ js/index.js
@@ -22,8 +22,8 @@ let result = document.getElementById('results').classList.remove('hidden')
 })
 
 
-let calculateSavingBtn = document.getElementById('calculate-savings')
-    .addEventListener('click', function(){
+let calculateSavingBtn = document.getElementById('calculate-savings');
+    calculateSavingBtn.addEventListener('click', function(){
 let savingInputField = parseFloat(document.getElementById('savings').value);
 let income = document.getElementById('income').value;
 let incomeNum=parseFloat(income);
@@ -43,3 +43,19 @@ let savingAmountValue = document.getElementById('savings-amount').innerText = sa
 
 let remainingBalanceAmount = document.getElementById('remaining-balance').innerText = remainingBalance.toFixed(2);
     }) 
+
+
+
+
+let historyTab = document.getElementById('history-tab');
+let assistantTab = document.getElementById('assistant-tab');
+historyTab.addEventListener('click', function(){
+
+        historyTab.classList.add('text-white', 'bg-gradient-to-r', 'from-blue-500', 'to-purple-600');
+
+        historyTab.classList.remove('text-gray-600')
+        assistantTab.classList.remove('text-white', 'bg-gradient-to-r', 'from-blue-500', 'to-purple-600')
+        assistantTab.classList.remove('text-gray-600')
+
+        document.getElementById('expense-form').classList.add('hidden')
+    })
